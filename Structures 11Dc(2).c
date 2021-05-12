24ae757ff231e2e421a1e8aa57a340af711d678d
cheenmaya-bhutad <55523581+cheenmaya-bhutad@users.noreply.github.com>
2021-05-12 12:23:19 +0530

    Add files via upload

---
+++ Structures 11Dc(2).c
@@ -0,0 +1,60 @@
+/* An automobile company has the serial number for engine parts starting from AA0 to FF9.
+The other characteristics of parts to be specified in a structure are:
+Year of manufacture,
+material,
+and quantity manufactured.
+(a) Specify a structure to store information corresponding to a part.
+(b) Write a program to retrieve information on parts with serial numbers between BB1 and CC6.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 50
+
+struct engineparts
+{
+    char serialnum[3];
+    int YOM;
+    char material[MAX];
+    int quantity;
+};
+void printenginepartinfo(struct engineparts list[]);
+
+void printenginepartinfo(struct engineparts list[])
+{
+    int i=0;
+    for(int j=0;j<54;j++)
+    {
+        if((list[j].serialnum[0]=='B') && (list[j].serialnum[1]=='B') && (list[j].serialnum[1]=='1'))
+        {
+            i=j;
+            do
+            {
+                printf("Year of Manufacturing : %d\n", list[i].YOM);
+                printf("Quantity available : %d\n", list[i].quantity);
+                printf("Serial number : %s\n", list[i].serialnum);
+                printf("Material Composition : %s", list[i].material);
+                i++;
+            }while((list[i].serialnum[0]!='C') && (list[i].serialnum[1]!='C') && (list[i].serialnum[1]!='6'));
+        }
+        else
+        {
+            continue;
+        }
+    }
+}
+
+int main()
+{
+    struct engineparts list[54];
+    for(int i=0;i<54;i++)
+    {
+        scanf("%d", &list[i].YOM);
+        scanf("%d", &list[i].quantity);
+        scanf("%s\n", list[i].serialnum);
+        fgets(list[i].material,MAX,stdin);
+    }
+    printenginepartinfo(list);
+    return 0;
+}
